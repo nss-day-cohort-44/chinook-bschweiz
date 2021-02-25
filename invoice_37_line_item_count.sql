8f2d6fe74cb66d1fbaea48d075b6ffa49f0d3b81
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-25 15:45:03 -0600

    q10 done

---
+++ invoice_37_line_item_count.sql
@@ -0,0 +1,9 @@
+1. `invoice_37_line_item_count.sql`: Looking at the InvoiceLine table, provide a query that 
+COUNTs the number of line items for 
+Invoice ID 37.
+
+
+SELECT InvoiceId, COUNT(InvoiceLineId)
+FROM InvoiceLine
+WHERE InvoiceId = 37
+GROUP BY InvoiceId;
