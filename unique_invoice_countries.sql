006e3f27d7876424b40dabea4827a3d25f13bbdf
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-25 14:34:28 -0600

    q5 solved

---
+++ unique_invoice_countries.sql
@@ -0,0 +1,5 @@
+1. `unique_invoice_countries.sql`: Provide a query showing a unique/distinct 
+list of billing countries from the Invoice table.
+
+SELECT DISTINCT BillingCountry 
+    FROM Invoice I;
