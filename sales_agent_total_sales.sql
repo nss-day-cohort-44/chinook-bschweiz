a76f47894712a4615cc83fc03e06372374ab0369
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-26 12:23:56 -0600

    working solution to q18 written

--- sales_agent_total_sales.sql
+++ sales_agent_total_sales.sql
@@ -1,1 +1,11 @@
-18.
+18.`sales_agent_total_sales.sql`: Provide a query that shows 
+total sales made by 
+each sales agent.
+
+SELECT C.FirstName RepName, C.LastName RepLast, SUM(I.Total)
+FROM Invoice I
+JOIN Customer C
+ON I.CustomerId = C.CustomerId
+JOIN Employee E
+ON E.EmployeeId = C.SupportRepId
+GROUP BY RepName;
