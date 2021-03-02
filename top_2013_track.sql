0dd5b1520e5a6ca16013611dbdeee7de0744a636
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-02 12:00:44 -0600

    hacking away at q24 on my own trying to figure it out without using Steve code from Slack, no real progress. stepping away for lunch break

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -1,7 +1,20 @@
 24. `top_2013_track.sql`: Provide a query that shows the most purchased track of 2013.
 
 
-SELECT
+SELECT t.Name AS TrackTitle, il.InvoiceLineId, i.InvoiceDate
+
+FROM
+    Invoice i
+    JOIN InvoiceLine il 
+    ON il.InvoiceId = i.InvoiceId
+    JOIN Track t
+    ON t.TrackId = il.TrackId
+
+    WHERE i.InvoiceDate LIKE '2013%'
+
+    GROUP BY TrackTitle
+
+
 -- steve's code: 
 with TopTracks as (
     select t.Name,
