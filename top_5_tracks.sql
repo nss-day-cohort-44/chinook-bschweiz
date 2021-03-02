992db522d1571ebf0f4b99fa5137a4bc72de31c0
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-02 14:42:23 -0600

    q25 done

--- top_5_tracks.sql
+++ top_5_tracks.sql
@@ -1,1 +1,18 @@
-25. `top_5_tracks.sql`: Provide a query that shows the top 5 most purchased tracks over all.
+25. `top_5_tracks.sql`: Provide a query that shows the 
+top 5 most purchased tracks over all.
+
+
+SELECT t.Name AS TrackTitle, 
+        COUNT(il.InvoiceLineId) AS BuyCount, i.InvoiceDate
+
+FROM
+    Invoice i
+    JOIN InvoiceLine il 
+    ON il.InvoiceId = i.InvoiceId
+    JOIN Track t
+    ON t.TrackId = il.TrackId
+
+    GROUP BY TrackTitle
+    ORDER BY BuyCount DESC
+    LIMIT 5;
+
