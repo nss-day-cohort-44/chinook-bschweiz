1f11941af195e36935482417356eaa8172fc3e14
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-02 11:00:41 -0600

    finally figured out q23, going back to refactor q22 now

--- top_country.sql
+++ top_country.sql
@@ -1,10 +1,32 @@
 23. `top_country.sql`: Which countrys customers spent the most?
 
 
-SELECT (TotalSales), Country
-FROM (
-    SELECT '$'||printf('%.2f',SUM(Total)) AS TotalSales, c.Country
-FROM
-Customer c,
-Invoice i)
-GROUP BY Country;
+
+
+
+SELECT '$'||printf('%.2f',MAX(Total)) AS TotalSales, Country
+
+    FROM (
+        SELECT
+        c.Country AS Country,
+        SUM(Total) as Total
+
+        FROM
+        Customer c
+        JOIN
+        Invoice i ON i.CustomerId = c.CustomerId
+GROUP BY c.Country
+    )
+
+
+SELECT
+MAX(Total),
+Country
+FROM(
+    SELECT
+    c.Country as Country,
+    Sum(i.total) as Total
+    FROM Customer c
+    JOIN Invoice i ON i.CustomerId = c.CustomerId
+    Group By c.country
+)
